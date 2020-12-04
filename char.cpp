35ab7d82e4545d194d997ef3605d949191513464
Brandon Huff <bh480818@ohio.edu>
2020-12-03 18:42:49 -0800

    program checked in

---
+++ char.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string s;
+    cout << "enter a string " << endl;
+    cin >> s;
+    cout << s.size() << " characters are in the string " << endl;
+    return 0;
+}
+
